cd9001a212b18e3a54322250054b2c3915fdbe5e
pavel_hladki <pavel_hladki@senla.eu>
2024-03-09 18:41:56 +0300

    docs: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import 'dotenv/config';
+import { generateSwaggerConfig } from './configs/swagger.config';
 
 const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+
+  SwaggerModule.setup('doc', app, generateSwaggerConfig(app));
+
   await app.listen(PORT);
 }
 bootstrap();
